d43f2ab2cca40f0b1fab8fcbb3291854726457bc
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-04 00:40:24 -0300

    :art: add default profile image

--- app/Profile.php
+++ app/Profile.php
@@ -10,6 +10,12 @@ class Profile extends Model
         'title', 'description', 'url', 'image'
     ];
 
+    public function profileImage()
+    {
+        $path = $this->image ? $this->image : "profile/0kw9UuVfyLDeWnAlDwmyvMY9gOqgDz3GbAdaELEI.jpeg";
+        return("/storage/{$path}");
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
