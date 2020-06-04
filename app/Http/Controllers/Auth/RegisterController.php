a16e5db6debd0de5c9615c6be48eac370a865002
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-03 23:56:47 -0300

    :fire: remove code

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,15 +66,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        $user = User::create([
+        return User::create([
             'name' => $data['name'],
             'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-        ]);
-        
-        $user->profile()->create();
-
-        return $user;
+        ]);        
     }
 }
