84b48660af32c015e645f37c5a2b7172ea150765
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-06-04 00:39:58 -0300

    :sparkles: create post's show page

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,15 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col-8">
-      <img src="/storage/{{$post->image}}" class="w-100">
+<div class="container">  
+    <div class="row">
+        <div class="col-8">
+            <img src="/storage/{{$post->image}}" class="w-100">
+        </div>
+        <div class="col-4">
+            <div>
+                <div class="d-flex align-items-center">
+                    <div class="pr-3">
+                        <img src="/storage/{{$post->user->profile->image}}" class="rounded-circle w-100" style="max-width: 50px">
+                    </div>
+
+                    <div>
+                        <div class="font-weight-bold">
+                            <a href="/{{$post->user->username}}">
+                                <span class="text-dark">{{$post->user->username}}</span>
+                            </a>
+                            
+                            <a class="pl-3">Follow</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <hr/> 
+
+                <p>
+                    <a href="/{{$post->user->username}}">
+                        <span class="font-weight-bold text-dark">{{$post->user->username}}</span>
+                    </a>
+
+                    {{$post->caption}}
+                </p>
+            </div>
+        </div>
     </div>
-    <div class="col-4">
-      <h3>{{$post->user->username}}</h3>
-      <p>{{$post->caption}}</p>
-    </div>
-  </div>
 </div>
 @endsection
