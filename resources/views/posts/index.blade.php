d5983e0334523825d827b4be73aeb426197a184b
tales <talesdsp@gmail.com>
2020-12-10 03:42:15 -0300

    add posts views

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,53 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">  
-  
-    @foreach ($posts as $post)
-    
-        <div class="row justify-content-center align-items-center flex-column pb-4">
-            
-            <div class="col-xl-5 col-lg-6 col-md-7 col-sm-12">
 
-                <div class="d-flex align-items-baseline pb-2">
-                    <div class="pr-3">
-                        <a href="/{{$post->user->username}}">
-                            <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 40px">
-                        </a>
-                    </div>
-    
-                    <div>
-                        <div class="font-weight-bold">
-                            <a href="/{{$post->user->username}}">
-                                <span class="text-dark">{{$post->user->username}}</span>
-                            </a>
-                            
-                        </div>
-                    </div>
-                </div>
-                                
-                <a href="/p/{{$post->id}}">
-                    <img src="/storage/{{$post->image}}" class="w-100">
-                </a>
-            </div>
-        
-            <div class="col-xl-5 col-lg-6 col-md-7 col-sm-12 pt-3">
-                <p>
-                    <a href="/{{$post->user->username}}">
-                        <span class="font-weight-bold text-dark">{{$post->user->username}}</span>
-                    </a>
+    <div class="index-stories sticky flex">
+        <a class="stories feed off ml-4 mb-4" href="/s/create">
+            <img src="/storage/{{$user->profile->image}}" />
+            <div class="plus-button">+</div>
+        </a>
 
-                    {{$post->caption}}
-                </p>
-            
-            </div>
-        </div>
-    
-    @endforeach
-
-    <div class="row col-12">
-        {{$posts->links()}}
+        @if(count($profilesWithStories['data'])>0)
+            <stories-section :user="{{$user}}" :stories="{{json_encode($profilesWithStories)}}"></stories-section>
+        @endif
     </div>
-    
-</div>
+
+    @if(count($posts['data'])>0)
+        <index-posts :posts="{{json_encode($posts)}}"></index-posts>
+    @endif
+
+    <recommend-profiles :profiles="{{json_encode($profilesIDoNotFollow)}}"></recommend-profiles>
 @endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,40 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">  
-    <div class="row justify-content-center ">
-        <div class="col-6">
-            <img src="/storage/{{$post->image}}" class="w-100">
-        </div>
-        <div class="col-4 pt-4">
-            <div>
-                <div class="d-flex align-items-center">
-                    <div class="pr-3">
-                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 50px">
-                    </div>
+<div class="container">
+    <post-info :auth-user="{{auth()->user() ? auth()->user(): json_encode(['id'=>'0'])}}" :same-user="{{(!Auth::check() ? 0 : auth()->user()->id == $user->id) ? 1 : 0}}" :profile="{{$profile}}" :user="{{$user}}" :post="post" :comments="{{json_encode($comments)}}"></post-info>
 
-                    <div>
-                        <div class="font-weight-bold">
-                            <a href="/{{$post->user->username}}">
-                                <span class="text-dark">{{$post->user->username}}</span>
-                            </a>
-                            
-                            <a class="pl-3">Follow</a>
-                        </div>
-                    </div>
-                </div>
-                
-                <hr/> 
+    @if(!Auth::check())
+        <log-banner></log-banner>
+    @endif
 
-                <p>
-                    <a href="/{{$post->user->username}}">
-                        <span class="font-weight-bold text-dark">{{$post->user->username}}</span>
-                    </a>
-
-                    {{$post->caption}}
-                </p>
-            </div>
-        </div>
-    </div>
+    <script type="application/javascript">
+        window.post = @json($post);
+    </script>
 </div>
 @endsection
